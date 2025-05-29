a181da8a4784611f156d74fff136d03ac7b4afcf
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-05-29 17:32:54 -0500

    🚀 **feat:** Implementar carga diferida con `React.lazy` y suspense
    - Actualizado `router.tsx` para usar `React.lazy` en la carga de `IndexPage` y `FavoritesPage`.
    - Añadido `Suspense` con un indicador de carga para mejorar la experiencia del usuario.

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,25 @@
+import {lazy, Suspense} from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import IndexPage from "./views/IndexPage.tsx";
-import FavoritesPage from "./views/FavoritesPage.tsx";
 import Layout from "./layouts/Layout.tsx";
+
+const FavoritesPage = lazy(() => import("./views/FavoritesPage.tsx"))
+const IndexPage = lazy(() => import("./views/IndexPage.tsx"))
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout/>}>
-          <Route path="/" element={<IndexPage/>} index/>
-          <Route path="/favoritos" element={<FavoritesPage/>}/>
+          <Route path="/" element={
+            <Suspense fallback={<div>Cargando...</div>}>
+              <IndexPage/>
+            </Suspense>
+          } index/>
+          <Route path="/favoritos" element={
+            <Suspense fallback={<div>Cargando...</div>}>
+              <FavoritesPage/>
+            </Suspense>
+          }/>
         </Route>
       </Routes>
     </BrowserRouter>
